9767138a454db7e2b6267f092f70b94657b78951
Talhashahar <talhashahar@gmail.com>
2018-04-30 19:24:12 +0300

    Finish Textbox model

--- Source.cpp
+++ Source.cpp
@@ -10,9 +10,9 @@ using namespace std;
 int main(int argc, char** argv) {
 
 	HANDLE in = GetStdHandle(STD_INPUT_HANDLE);
-	Textbox chk({ 4,4 }, "Zibi", 10, 4);
+	Textbox chk({ 4,4 }, "Fifa 18", 10, 4);
 
-	chk.draw1();
+	chk.draw();
 	int counter = 0;
 	while (1) {
 		INPUT_RECORD ir;
